0d96419e034891ba77b2738805b4ab2d37e640be
Brian Nkhata <bnkhata@mra.mw>
2025-01-23 13:46:14 +0200

    WIP

--- HR.LeaveManagement.Application/ApplicationServiceRegistration.cs
+++ HR.LeaveManagement.Application/ApplicationServiceRegistration.cs
@@ -1,16 +1,16 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace HR.LeaveManagement.Application
 {
     public static class ApplicationServiceRegistration
     {
-        public static void ConfigureApplicationServices(this IServiceCollection services)
+        public static IServiceCollection ConfigureApplicationServices(this IServiceCollection services)
         {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            return services;
 
         }
     }
