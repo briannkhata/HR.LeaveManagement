050a3a52afeb1d95478c27c03381c1731dd4dc6d
Brian Nkhata <bnkhata@mra.mw>
2025-01-23 13:30:43 +0200

    Add project files.

---
+++ HR.LeaveManagement.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static void ConfigureApplicationServices(this IServiceCollection services)
+        {
+
+        }
+    }
+}
---
+++ HR.LeaveManagement.Application/DTOs/LeaveRequest/LeaveRequestListDto.cs
@@ -0,0 +1,13 @@
+﻿using HR.LeaveManagement.Application.DTOs.Common;
+
+namespace HR.LeaveManagement.Application.DTOs
+{
+    public class LeaveRequestListDto : BaseDto
+    {
+
+        public DateTime DateRequested { get; set; }
+        public string RequestComments { get; set; }
+        public DateTime DateActioned { get; set; }
+        public bool? Approved { get; set; }
+    }
+}
